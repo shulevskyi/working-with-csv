fd83ebf626108d14460e32de4f2eb39d36583708
shulevskyi <dshulevskyi@kse.org.ua>
2021-10-06 11:38:50 +0300

    stage-2

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ added_data = []
 all_medals = []
 data_of_players = []
 count_medals = 0
-a = 0
-b = 3
+i = 0
 result_everything = None
 
 # Declaring arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=str)
+group = parser.add_mutually_exclusive_group()  # There is a group thanks to user has 2 different option to choose
+group.add_argument('--total', action='store_true')
+group.add_argument('--medals', action='store_true')
 parser.add_argument('country', type=str)
 parser.add_argument('year', type=int)
 parser.add_argument('output_file', type=str)
 args = parser.parse_args()
+
+if args.total:
+    parser.add_argument('country', help=argparse.SUPPRESS)
+
 
 with open('data.csv') as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -26,41 +32,69 @@ with open('data.csv') as csvfile:
             year = int(row[9])  # converting str year to int
         except ValueError:
             continue
-        if args.year == year and args.country in row[6]:
-            all_medals.append(row[-1])
+        if args.total:
+            if args.year == year:
+                all_medals.append(row[-1])
 
-            # Getting info from rows: name, activity type, medals
-            data_of_players.append(row[1])
-            data_of_players.append(row[12])
-            data_of_players.append(row[-1])
-            result_everything = True
+                data_of_players.append(row[6])
+                data_of_players.append(row[-1])
+        if args.medals:
+            if args.year == year and args.country in row[6]:
+                all_medals.append(row[-1])
 
-    file = open(args.output_file, 'w')  # Creating a file, where output will be printed
-    if not result_everything:
-        print('There is wrong year or name of country')
-        file.write('There is wrong year or name of country')
-        exit()
+                # Getting info from rows: name, activity type, medals
+                data_of_players.append(row[1])
+                data_of_players.append(row[12])
+                data_of_players.append(row[-1])
+                result_everything = True
 
-    else:
-        for i in range(0, len(all_medals)):
-            if 'NA' != all_medals[i]:  # Calculating if there is any medals except NA
-                count_medals += 1
+    if args.total:
+        a = 0
+        b = 2
+        while b < len(data_of_players):
+            added_data.append(data_of_players[a:b])
+            a += 2
+            b += 2
 
-        while True:
-            added_data.append(data_of_players[a:b])  # Starting from the new line
-            a += 3
-            b += 3
-            if len(added_data) == 10:
-                break
-        final_list = [x for x in added_data if x != []]  # Deleting empty lists if this is necessary
+        for i in added_data:
+            if 'NA' in i[1]:
+                i.clear()
+        final_list = [x for x in added_data if x != []]
+        file = open(args.output_file, 'w')
         for i in range(0, len(final_list)):
+            file = open(args.output_file, 'w')
+            file.write(' - '.join(final_list[i]))
             print(' - '.join(final_list[i]))
-            file.write(' - '.join(final_list[i]) + "\n")
 
-        if count_medals < 10:
-            print('\nCountry has less than 10 medals')
-            file.write('\nCountry has less than 10 medals')
-        if count_medals >= 10:
-            print(f'\nTotal number of medals: {count_medals}')
-            file.write(f'\nTotal number of medals: {count_medals}')
-        file.close()
+    if args.medals:
+        file = open(args.output_file, 'w')  # Creating a file, where output will be printed
+        if not result_everything:
+            print('There is wrong year or name of country')
+            file.write('There is wrong year or name of country')
+            exit()
+
+        else:
+            for i in range(0, len(all_medals)):
+                if 'NA' != all_medals[i]:  # Calculating if there is any medals except NA
+                    count_medals += 1
+
+            a = 0
+            b = 3
+            while True:
+                added_data.append(data_of_players[a:b])  # Starting from the new line
+                a += 3
+                b += 3
+                if len(added_data) == 10:
+                    break
+            final_list = [x for x in added_data if x != []]  # Deleting empty lists if this is necessary
+            for i in range(0, len(final_list)):
+                print(' - '.join(final_list[i]))
+                file.write(' - '.join(final_list[i]) + "\n")
+
+            if count_medals < 10:
+                print('\nCountry has less than 10 medals')
+                file.write('\nCountry has less than 10 medals')
+            if count_medals >= 10:
+                print(f'\nTotal number of medals: {count_medals}')
+                file.write(f'\nTotal number of medals: {count_medals}')
+            file.close()
